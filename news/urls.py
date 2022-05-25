415bcffdb21b95022d3a9ef41f0360518e693cce
96olbap <alvinwanjala@gmail.com>
2022-05-25 11:15:04 +0300

    This commit will add the articles urlpattern

--- news/urls.py
+++ news/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     re_path(r'^$', views.news_of_day, name='newsToday'),
     re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
-    re_path(r'^search/', views.search_results, name = 'search_results')
+    re_path(r'^search/', views.search_results, name = 'search_results'),
+    re_path(r'^article/(\d+)', views.article, name='article')
 ]
 
 if settings.DEBUG:
