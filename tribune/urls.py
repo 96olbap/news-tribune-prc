cc2893748b607d401935c525d60adb774f6f123a
96olbap <alvinwanjala@gmail.com>
2022-06-04 00:03:43 +0300

    This commit will update user registration

--- tribune/urls.py
+++ tribune/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'',include('news.urls')),
     re_path(r'^accounts/', include('registration.backends.simple.urls')),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), {"next_page": '/'}),
 ]
